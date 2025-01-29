4da027818b6e60d49b5ac0703e0abdcd0581adab
Lewis Mander <lewis.mander@googlemail.com>
2025-01-29 10:55:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// index.js
-// where your node app starts
-
-// init project
 const path = require('path');
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,8 +15,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
